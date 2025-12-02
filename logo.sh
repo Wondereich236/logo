3097c3b8357dfe8db8e81bf208f321a42bca50d0
Wondereich236 <kiyotakaa591@gmail.com>
2025-12-02 13:44:07 +0300

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -10,7 +10,10 @@ sudo cp ~/git-script/logo/*.{asc,png} /usr/share/SatellaOS/logo/
 # Dosya izinlerini ayarla
 sudo chmod 655 /usr/share/SatellaOS/logo/*
 
-# Kullanıcı Pictures klasörüne sembolik link oluştur
-ln -s /usr/share/SatellaOS/logo "$(xdg-user-dir PICTURES)/logo" 2>/dev/null
+# Kullanıcı için satella-picture klasörünü oluştur
+mkdir -p ~/satella-picture
+
+# Bu klasöre sembolik link ekle
+ln -s /usr/share/SatellaOS/logo ~/satella-picture/logo 2>/dev/null
 
 echo "✔ Logo başarıyla yüklendi!"
