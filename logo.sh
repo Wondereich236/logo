10672194631ee4cb024867076d2abda88a115110
satella <kiyotakaa591@gmail.com>
2025-12-04 15:42:21 +0300

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 shopt -s nullglob
 
-# SatellaOS logo dizinini oluştur
 sudo mkdir -p /usr/share/SatellaOS/logo/
 
-# Logo dosyalarını kopyala
 sudo cp ~/git-script/logo/*.{asc,png} /usr/share/SatellaOS/logo/
 
-# Dosya izinlerini ayarla
 sudo chmod 655 /usr/share/SatellaOS/logo/*
 
-# Kullanıcı için satella-picture klasörünü oluştur
 mkdir -p ~/satella-picture
 
-# Bu klasöre sembolik link ekle
 ln -s /usr/share/SatellaOS/logo ~/satella-picture/logo 2>/dev/null
-
-echo "✔ Logo başarıyla yüklendi!"
